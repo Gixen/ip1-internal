6bc7648382f257c6d4f5272ce20225c00435e52d
Gixen <gix.neidhaart@gmail.com>
2022-06-21 14:02:41 +0200

    Add and config angular app

---
+++ ip1-internal/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace IP1.Internal
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
